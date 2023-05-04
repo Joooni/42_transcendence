7ec56059d978c769d9f61eec40f9ee1bc661dbd8
Saskia Mischnick <smischni@student.42wolfsburg.de>
2023-05-04 19:31:55 +0200

    css Home

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -4,8 +4,24 @@ function Home () {
 		<p>
 			WELCOME saskia! YOU STILL HAVE A LOT TO LEARN!
 		</p>
-		<div className="Button">
-			JOIN A NEW GAME
+		<div className = "flex_row">
+			<div className = "flex_column">
+				<div className = "Button">JOIN A GAME</div>
+				<h3 className = "headline">ONGOING GAMES</h3>
+				<div className = "ongoing_game">saskia vs. jonas</div>
+				<div className = "ongoing_game">saskia vs. jonas</div>
+				<div className = "ongoing_game">saskia vs. jonas</div>
+				<div className = "ongoing_game">saskia vs. jonas</div>
+				<div className = "ongoing_game">saskia vs. jonas</div>
+				<div className = "ongoing_game">saskia vs. jonas</div>
+				<div className = "ongoing_game">saskia vs. jonas</div>
+				<div className = "ongoing_game">saskia vs. jonas</div>
+				<div className = "ongoing_game">saskia vs. jonas</div>
+				<div className = "ongoing_game">saskia vs. jonas</div>
+				<div className = "ongoing_game">saskia vs. jonas</div>
+				<div className = "ongoing_game">saskia vs. jonas</div>
+			</div>
+			<div className = "flex_column">JOIN A SECOND GAME</div>
 		</div>
 	  </div>
 	);
