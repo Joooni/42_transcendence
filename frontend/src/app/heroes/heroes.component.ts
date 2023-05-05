da271490108c8fd09196e2d4e2fba262d5000d26
smischni <saskia.mischnick@gmail.com>
2023-05-05 20:32:37 +0200

    Working Tour of Heroes

--- frontend/src/app/heroes/heroes.component.ts
+++ frontend/src/app/heroes/heroes.component.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
-  heroes = HEROES;
-  selectedHero?: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+export class HeroesComponent implements OnInit {
+	heroes: Hero[] = [];
+  
+	constructor(private heroService: HeroService) { }
+  
+	ngOnInit(): void {
+	  this.getHeroes();
+	}
+  
+	getHeroes(): void {
+	  this.heroService.getHeroes()
+	  .subscribe(heroes => this.heroes = heroes);
+	}
   }
-}
