2c411967d65824cfa967ef63c2490a00a8e0d7cf
Jonas Subel <joooni.su@gmail.com>
2023-05-08 17:55:19 +0200

    routes figured out, now on to the actual token stuff

--- backend/src/auth/controller/auth.controller.ts
+++ backend/src/auth/controller/auth.controller.ts
@@ -1,7 +1,9 @@
 
-import { Body, Controller, Post, HttpCode, HttpStatus, Get, Req, Res } from '@nestjs/common';
+import { Body, Controller, Post, HttpCode, HttpStatus, Get, Req, Res, BadRequestException, UseGuards } from '@nestjs/common';
 import { AuthService } from '../service/auth.service';
-
+import { Response } from 'express';
+import { CreateUserInput } from 'src/users/dto/create-user.input';
+import { AuthGuard } from '@nestjs/passport';
 @Controller('42intra')
 export class AuthController {
 	constructor(private authService: AuthService) {}
@@ -10,7 +12,23 @@ export class AuthController {
 	// ideally, instead of Record, use a DTO class to define request body shape
 	@Get('login')
 	async intra_login(@Req() req: any, @Res() res: any): Promise<any> {
+		console.log("inside auth controller/login");
 		await this.authService.login(req, res);
 	}
 
+	@Get('callback')
+	async callback(@Req() req: any, @Res({ passthrough: true }) res: Response) {
+		if (typeof req.user == 'undefined')
+			throw new BadRequestException("Verification with 42Intra failed");
+	}
+
+	@Get('test')
+	testfunc(): string {
+		return "this is a teststring";
+	}
+
+	@Get()
+	headempty(): string {
+		return "this is just /42intra, bruh";
+	}
 }
--- backend/src/auth/service/auth.service.ts
+++ backend/src/auth/service/auth.service.ts
@@ -13,36 +13,15 @@ export class AuthService {
 
   async login(@Req() _req: any, @Res() _res: any): Promise<any> {
     var user = await this.usersService.findOne(_req.user.id);
-
-    if(user.twoFAEnabled)
-    {
-      // two FA route goes here
-
-      return ;
-    }
+    if (!user)
+      // return or throw erorr
+    // if(user.twoFAEnabled == true)
+    // {
+    //   // two FA route goes here
+    //   return user;
+    // }
     // Intra Login goes here
     return ;
     // should return a 42 signed JWT token
-  }
-
-
-  create(createAuthInput: CreateAuthInput) {
-    return 'This action adds a new auth';
-  }
-
-  findAll() {
-    return `This action returns all auth`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthInput: UpdateAuthInput) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
   }
 }
