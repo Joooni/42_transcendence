8b3663fd77dbc9842c3d65041c66c7d9fcaefb2c
Jonas Subel <joooni.su@gmail.com>
2023-05-25 14:51:03 +0200

    error whoops

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -2,8 +2,5 @@ import { Controller } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
-  constructor(
-    private readonly usersService: UsersService,
-    private readonly configService: ConfigService,
-  ) {}
+  constructor() {}
 }
