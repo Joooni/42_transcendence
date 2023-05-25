8c2690df95150a113805a8a22e0535ed2227fb78
Jonas Subel <joooni.su@gmail.com>
2023-05-25 12:19:41 +0200

    added 2FA service and controller - need to figure out how to make a QR code with the library

---
+++ backend/src/auth/controller/twoFA.controller.ts
@@ -0,0 +1,76 @@
+import { BadRequestException, Controller, Req, UseGuards, Get, Res, Query } from '@nestjs/common';
+import { JwtAuthGuard } from '../guard/jwt.guard';
+import { JwtService } from '@nestjs/jwt';
+import { TwoFAService } from '../service/twoFA.service';
+import { UsersService } from '../../users/users.service';
+import { User } from 'src/users/entities/user.entity';
+import { JwtPayload } from '../strategy/jwt.strategy';
+
+
+@Controller('2fa')
+export class TwoFAController {
+	constructor(
+		private readonly twoFAService: TwoFAService,
+		private readonly jwtService: JwtService,
+		private readonly usersService: UsersService,
+	) {}
+
+	@Get('generate')
+	@UseGuards(JwtAuthGuard)
+	async generate(@Req() req: any, @Res() res: Response): Promise<void> {
+		try {
+			const user: User = await this.usersService.findOne(req.user.id);
+			const { otpAuthUrl } = await this.twoFAService.generate2FASecret(user);
+			// learn how to make QR code and/or secret passphrase for authenticator app here
+			return ;
+		} catch (error) {
+			throw new BadRequestException(error.message);
+		}
+	}
+
+	@Get('verify')
+	@UseGuards(JwtAuthGuard)
+	async verify(@Req() req: any, @Res() res: Response, @Query('code') code: string | null) {
+		if (req.user.isAuthenticated)
+			throw new BadRequestException('User is already authenticated');
+		if (!code)
+			throw new BadRequestException('2FA code missing');
+		try {
+			const user: User = await this.usersService.findOne(req.user.id);
+			return await this.twoFAService.verify2FA(user, code).then(() => {
+				const jwt_token = this.jwtService.sign({
+					id: req.user.id,
+					email: req.user.email,
+					isAuthenticated: true,
+				} as JwtPayload)
+			})
+		} catch (error) {
+			throw new BadRequestException(error.message);
+		}
+	}
+
+
+	@Get('enable')
+	@UseGuards(JwtAuthGuard)
+	async enableTwoFA(@Req() req: any, @Query('code') code: string | null): Promise<void> {
+		if (!code)
+			throw new BadRequestException('2FA code missing');
+		try {
+			const user: User = await this.usersService.findOne(req.user.id);
+			await this.twoFAService.enable2FA(user, code);
+		} catch (error) {
+			throw new BadRequestException(error.message);
+		}
+	}
+
+	@Get('disable')
+	@UseGuards(JwtAuthGuard)
+	async disableTwoFA(@Req() req: any): Promise<void> {
+		try {
+			const user: User = await this.usersService.findOne(req.user.id);
+			await this.twoFAService.disable2FA(user);
+		} catch (error) {
+			throw new BadRequestException(error.message);
+		}
+	}
+}
