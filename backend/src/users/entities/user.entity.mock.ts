952ee1cebac1bcf45c68ab65b3df0f23e94e3b99
Triinu Jairus <tjairus@students.42wolfsburg.de>
2023-06-21 10:32:31 +0300

    minor fix

--- backend/src/users/entities/user.entity.mock.ts
+++ backend/src/users/entities/user.entity.mock.ts
@@ -32,17 +32,7 @@ export const mockUser1: User = createMockUser({
 });
 
 export const mockUsers: User[] = [
-  createMockUser({
-    id: 4269,
-    intra: 'dokong',
-    firstname: 'Donkey',
-    lastname: 'Kong',
-    username: 'LoveBarrelsHateMario',
-    email: 'redtiesrule@kongs.com',
-    picture: 'https://mario.wiki.gallery/images/8/84/MPS_Donkey_Kong_Artwork.png',
-    wins: 69,
-    losses: 42,
-  }),
+
   createMockUser({
     id: 6942,
     intra: 'dikong',
@@ -53,5 +43,27 @@ export const mockUsers: User[] = [
     picture: 'https://www.mariowiki.com/Diddy_Kong#/media/File:DiddyKong2.png',
     wins: 42,
     losses: 69,
+  }),
+  createMockUser({
+    id: 6940,
+    intra: 'dmake kong',
+    firstname: 'Diddy',
+    lastname: 'Kong',
+    username: 'BongosAre',
+    email: 'redcapsle@kongs.com',
+    picture: 'https://www.mariowiki.com/Diddy_Kong#/media/File:DiddyKong2.png',
+    wins: 4,
+    losses: 9,
+  }),
+  createMockUser({
+    id: 6980,
+    intra: 'dmke kong',
+    firstname: 'Di',
+    lastname: 'Kong',
+    username: 'BongosAre?',
+    email: 'redcapsle@kongs.com',
+    picture: 'https://www.mariowiki.com/Diddy_Kong#/media/File:DiddyKong2.png',
+    wins: 41,
+    losses: 9,
   }),
 ];
