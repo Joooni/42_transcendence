d4cd284710b1bddca9934e1c694813c09527c4cc
Jonas Subel <jsubel@students.42wolfsburg.de>
2023-07-10 12:13:11 +0200

    added mockusers to database

--- backend/src/users/entities/user.entity.mock.ts
+++ backend/src/users/entities/user.entity.mock.ts
@@ -31,15 +31,49 @@ export const mockUser1: User = createMockUser({
   losses: 42,
 });
 
-export const mockUser2: User = createMockUser({
-  id: 6942,
-  intra: 'dikong',
-  firstname: 'Diddy',
-  lastname: 'Kong',
-  username: 'BongosAreMyJam',
-  email: 'redcapsrule@kongs.com',
-  picture:
-    'https://mario.wiki.gallery/images/thumb/0/06/DiddyKong2.png/1200px-DiddyKong2.png',
-  wins: 42,
-  losses: 69,
-});
+export const mockUsers: User[] = [
+  createMockUser({
+    id: 1,
+    intra: 'dikong',
+    firstname: 'Diddy',
+    lastname: 'Kong',
+    username: 'RedCapsRule',
+    email: 'redcapsle@kongs.com',
+    picture: 'https://www.mariowiki.com/Diddy_Kong#/media/File:DiddyKong2.png',
+    wins: 4,
+    losses: 9,
+  }),
+  createMockUser({
+    id: 2,
+    intra: 'lakong',
+    firstname: 'Lanky',
+    lastname: 'Kong',
+    username: 'LongArmMaster',
+    email: 'lanky@kongs.com',
+    picture: 'https://mario.wiki.gallery/images/thumb/b/bf/Lankyblast.jpg/1200px-Lankyblast.jpg',
+    wins: 41,
+    losses: 9,
+  }),
+  createMockUser({
+    id: 3,
+    intra: 'tikong',
+    firstname: 'Tiny',
+    lastname: 'Kong',
+    username: 'gurlpower',
+    email: 'tiny@kongs.com',
+    picture: 'https://mario.wiki.gallery/images/thumb/8/81/Tiny_Kong_MSS_art.png/200px-Tiny_Kong_MSS_art.png',
+    wins: 100,
+    losses: 100,
+  }),
+  createMockUser({
+    id: 4,
+    intra: 'chunkykong',
+    firstname: 'Chunky',
+    lastname: 'Kong',
+    username: 'Afraidycat',
+    email: 'chunky@kongs.com',
+    picture: 'https://mario.wiki.gallery/images/thumb/8/81/SSBU_Chunky_Kong_Spirit.png/200px-SSBU_Chunky_Kong_Spirit.png',
+    wins: 100,
+    losses: 100,
+  }),
+]
