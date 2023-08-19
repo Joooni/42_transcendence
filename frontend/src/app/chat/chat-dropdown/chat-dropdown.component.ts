f0b46bf304167e403b63e86124c8b5bafeb32ea1
smischni <saskia.mischnick@gmail.com>
2023-08-19 14:19:58 +0200

    contact drop down

--- frontend/src/app/chat/chat-dropdown/chat-dropdown.component.ts
+++ frontend/src/app/chat/chat-dropdown/chat-dropdown.component.ts
@@ -23,13 +23,42 @@ export class ChatDropdownComponent {
 		this.userDataService.findSelf().then(user => this.activeUser = user);
 	}
 
-	blocksOrIsBlockedByActiveUser(): boolean {
-		//TO-DO: check if the selectedUser blocked the other user or the other way around
-		//if so, return true
+	isBlockedByActiveUser(): boolean {
+		//TO-DO: check if active user is blocking that user
 		return false;
+	}
+
+	isFriendsWithActiveUser(): boolean {
+		return true;
 	}
 
 	openDMWithUser() {
 		this.chatComponent.selectUser(this.selectedUser);
+	}
+
+	sendGameRequest() {
+		console.log('send Game Request to ' + this.selectedUser);
+		//TO-DO: Function to send game request
+	}
+
+	sendFriendRequest() {
+		console.log('send Friend Request to ' + this.selectedUser);
+		//TO-DO: Function to send friend request
+	}
+
+	removeAsFriend() {
+		console.log('remove ' + this.selectedUser + ' as friend');
+		//TO-DO: Function to remove as friend
+	}
+
+	blockUser() {
+		console.log('block ' + this.selectedUser);
+		//TO-DO: Function to block someone
+		//when blocking someone, that person should automatically be unfriended as well, if necessary
+	}
+
+	unblockUser() {
+		console.log('unblock ' + this.selectedUser);
+		//TO-DO: Function to unblock someone
 	}
 }
